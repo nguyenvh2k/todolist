ef686fa3d6b00b37e818d58886145db4b42d957f
Nguyen <nguyenvh2k@gmail.com>
2023-02-17 18:01:14 +0700

    adbf

--- src/main/java/com/example/sb13/controller/RestApiController.java
+++ src/main/java/com/example/sb13/controller/RestApiController.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "https://nguyenvh2k.github.io/", allowedHeaders = "*")
 @Tag(name = "Todo")
 public class RestApiController {
     @Autowired
