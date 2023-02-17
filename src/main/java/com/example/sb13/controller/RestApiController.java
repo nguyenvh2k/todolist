b959ef5bb808371cef5f8dc107d3d851a0e55d1f
Nguyen <nguyenvh2k@gmail.com>
2023-02-17 17:43:42 +0700

    qua buc

--- src/main/java/com/example/sb13/controller/RestApiController.java
+++ src/main/java/com/example/sb13/controller/RestApiController.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "http://127.0.0.1:5500", allowedHeaders = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Tag(name = "Todo")
 public class RestApiController {
     @Autowired
